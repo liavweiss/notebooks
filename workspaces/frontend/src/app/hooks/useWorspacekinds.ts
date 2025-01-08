da396a0ce2127769a659ca005a1596d6e025f3e9
Liav Weiss (EXT-Nokia) <liav.weiss.ext@nokia.com>
2025-01-08 10:23:03 +0200

    Modify the Kind column to show an icon representing the WorkspaceKind using workspacekinds API.
    Signed-off-by: Liav Weiss (EXT-Nokia) <liav.weiss.ext@nokia.com>

--- workspaces/frontend/src/app/hooks/useWorspacekinds.ts
+++ workspaces/frontend/src/app/hooks/useWorspacekinds.ts
@@ -7,15 +7,12 @@ import { WorkspaceKind } from '~/shared/types';
 import { useNotebookAPI } from '~/app/hooks/useNotebookAPI';
 
 const useWorkspacekinds = (): FetchState<WorkspaceKind[]> => {
-    console.log("Im here from useWorkspacekinds")
   const { api, apiAvailable } = useNotebookAPI();
-  console.log("Im here after useWorkspacekinds!!!!")
   const call = React.useCallback<FetchStateCallbackPromise<WorkspaceKind[]>>(
     (opts) => {
       if (!apiAvailable) {
         return Promise.reject(new Error('API not yet available'));
       }
-      console.log("Im here before calling the api")
       return api.getWorkspacekinds(opts);
     },
     [api, apiAvailable],
